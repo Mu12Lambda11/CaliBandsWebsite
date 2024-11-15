aecd46687e249f90551469926173a7114b55a2ad
Mu12Lambda11 <89223075+Mu12Lambda11@users.noreply.github.com>
2024-11-15 15:47:31 -0800

    Removed sections in About

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,15 +20,8 @@ class About extends Component {
       <section id="about">
         <Fade duration={1000}>
           <div className="row">
-            <div className="three columns">
-              <img
-                className="profile-pic"
-                src={profilepic}
-                alt="Nordic Giant Profile Pic"
-              />
-            </div>
             <div className="nine columns main-col">
-              <h2>About Me</h2>
+              <h2>Mission Statement</h2>
 
               <p>{bio}</p>
               <div className="row">
@@ -48,13 +41,7 @@ class About extends Component {
                     <span>{email}</span>
                   </p>
                 </div>
-                <div className="columns download">
-                  <p>
-                    <a href={resumeDownload} className="button">
-                      <i className="fa fa-download"></i>Download Resume
-                    </a>
-                  </p>
-                </div>
+                
               </div>
             </div>
           </div>
