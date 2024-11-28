411e0d483eb477e9d25df694cfa377b0293e100d
Mu12Lambda11 <idjordan@cpp.edu>
2024-11-27 19:15:55 -0800

    Added mission statement

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,16 +5,9 @@ class About extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const name = this.props.data.name;
-    const profilepic = "images/" + this.props.data.image;
-    const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
-    const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
-    const email = this.props.data.email;
-    const resumeDownload = this.props.data.resumedownload;
+
+    const missionstatement = this.props.data.missionstatement;
+
 
     return (
       <section id="about">
@@ -23,26 +16,8 @@ class About extends Component {
             <div className="nine columns main-col">
               <h2>Mission Statement</h2>
 
-              <p>{bio}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name}</span>
-                    <br />
-                    <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
-                    </span>
-                    <br />
-                    <span>{phone}</span>
-                    <br />
-                    <span>{email}</span>
-                  </p>
-                </div>
-                
-              </div>
+              <p>{missionstatement}</p>
+              
             </div>
           </div>
         </Fade>
