b0406df17381a74c713c2c34d13b3a2c5428d6f2
Mu12Lambda11 <idjordan@cpp.edu>
2024-11-27 20:14:04 -0800

    Took out social media buttons in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,6 @@ class Footer extends Component {
         <div className="row">
           <Fade bottom>
             <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
                 <li>&copy; Copyright 2024 CaliBands</li>
